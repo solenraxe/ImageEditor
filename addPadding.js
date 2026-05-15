03da6ee41808d73e0832459fe3e919f86665a5a4
solenraxe <solenraxe@gmail.com>
2026-05-15 16:57:44 +0200

    Changing color now works as intended Started adding padding

---
+++ addPadding.js
@@ -0,0 +1,81 @@
+let mainWindow = document.getElementById("main");
+
+let canvas = document.getElementById("drawing-canvas");
+const ctx = canvas.getContext("2d", {willReadFrequently: true});
+
+let image = document.getElementById("source-img");
+image.src = "FlappyBird.png";
+image.crossOrigin = "anonymous";
+
+let imgINP = document.getElementById("img-input");
+imgINP.addEventListener("change", function() {
+    const files = imgINP.files;
+    if (files.length === 0) {return;}
+
+    const newIMG = files[0];
+
+    image.src = URL.createObjectURL(newIMG);
+
+    image.onload = function() {
+        const targetHeight = 200;
+        const dimRatio = targetHeight / image.naturalHeight;
+    
+        const targetWidth = Math.floor(image.naturalWidth * dimRatio);
+
+        image.style.width = targetWidth + "px";
+        image.style.height = targetHeight + "px";
+
+        image.width = targetWidth;
+        image.height = targetHeight;
+
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+
+        canvas.style.width = Math.floor(250 * (image.naturalWidth/image.naturalHeight)) + "px";
+
+        changeImageColor(image)
+    };
+})
+
+function addPadding(img, pX, pY) {
+    const prevW = img.naturalWidth;
+    const prevH = img.naturalHeight;
+    
+    ctx.drawImage(img, 0, 0);
+    const oldData = ctx.getImageData(0, 0, prevW, prevH).data;
+
+    canvas.width = prevW + (pX * 2);
+    canvas.height = prevH + (pY * 2);
+    
+    const newImgData = ctx.createImageData(canvas.width, canvas.height);
+    const newData = newImgData.data;
+
+    for (let y = 0; y < canvas.height; y++) {
+        for (let x = 0; x < canvas.width; x++) {
+            const newIndex = (y * canvas.width + x) * 4;
+
+            if (x >= pX && x < pX + prevW && y >= pY && y < pY + prevH) {
+                const oldX = x - pX;
+                const oldY = y - pY;
+                const oldIndex = (oldY * prevW + oldX) * 4;
+
+                newData[newIndex] = oldData[oldIndex];
+                newData[newIndex + 1] = oldData[oldIndex + 1];
+                newData[newIndex + 2] = oldData[oldIndex + 2];
+                newData[newIndex + 3] = oldData[oldIndex + 3];
+            } else {
+                newData[newIndex + 3] = 0; 
+            }
+        }
+    }
+    ctx.putImageData(newImgData, 0, 0);
+}
+
+function download() {
+  const dataURL = canvas.toDataURL("image/png");
+
+  const a = document.createElement("a");
+  a.href = dataURL;
+  a.download = "image.png";
+  a.click();
+}
--- changeColor.js
+++ changeColor.js
@@ -1,10 +1,3 @@
-let colorCodes = {
-    Red: [1, 0, 0],
-    Green: [0, 1, 0],
-    Blue: [0, 0, 1],
-    Gray: [1, 1, 1]
-};
-
 let mainWindow = document.getElementById("main");
 
 let canvas = document.getElementById("drawing-canvas");
@@ -13,6 +6,8 @@ const ctx = canvas.getContext("2d", {willReadFrequently: true});
 let image = document.getElementById("source-img");
 image.src = "FlappyBird.png";
 image.crossOrigin = "anonymous";
+
+let colorINP = document.getElementById("input-1")
 
 let imgINP = document.getElementById("img-input");
 imgINP.addEventListener("change", function() {
@@ -44,7 +39,25 @@ imgINP.addEventListener("change", function() {
     };
 })
 
+colorINP.addEventListener("change", function() {
+    changeImageColor(image)
+})
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 function changeImageColor(img) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let rgb = hexToRgb(colorINP.value)
+    const colorCodes = [rgb.r/255, rgb.g/255, rgb.b/255]
+
     ctx.drawImage(img, 0, 0);
     const imageData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
     const data = imageData.data;
@@ -57,40 +70,6 @@ function changeImageColor(img) {
     }
 
     ctx.putImageData(imageData, 0, 0);
-}
-
-function addPadding(img, pX, pY) {
-    const prevW = img.naturalWidth;
-    const prevH = img.naturalHeight;
-    
-    ctx.drawImage(img, 0, 0);
-    const oldData = ctx.getImageData(0, 0, prevW, prevH).data;
-
-    canvas.width = prevW + (pX * 2);
-    canvas.height = prevH + (pY * 2);
-    
-    const newImgData = ctx.createImageData(canvas.width, canvas.height);
-    const newData = newImgData.data;
-
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-            const newIndex = (y * canvas.width + x) * 4;
-
-            if (x >= pX && x < pX + prevW && y >= pY && y < pY + prevH) {
-                const oldX = x - pX;
-                const oldY = y - pY;
-                const oldIndex = (oldY * prevW + oldX) * 4;
-
-                newData[newIndex] = oldData[oldIndex];
-                newData[newIndex + 1] = oldData[oldIndex + 1];
-                newData[newIndex + 2] = oldData[oldIndex + 2];
-                newData[newIndex + 3] = oldData[oldIndex + 3];
-            } else {
-                newData[newIndex + 3] = 0; 
-            }
-        }
-    }
-    ctx.putImageData(newImgData, 0, 0);
 }
 
 function download() {
